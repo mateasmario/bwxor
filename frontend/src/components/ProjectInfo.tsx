1163fea4af1529e3112e7f645f2a1d65c5dd794e
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-03-15 13:07:36 +0200

    Fix

--- frontend/src/components/ProjectInfo.tsx
+++ frontend/src/components/ProjectInfo.tsx
@@ -1,19 +1,37 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import Markdown from "react-markdown";
 
 interface ProjectInfoProps {
-    projectId: string,
-    projectName: string,
-    description: string,
-    contributors: string,
-    releaseLog: string[],
+    name : string | undefined;
+    repo : string | undefined;
+    contributors : string[] | undefined
 }
 
 function ProjectInfo(props : ProjectInfoProps) {
     const [activeItem, setActiveItem] = useState("description");
 
+    const [description, setDescription] = useState("");
+
+    useEffect(() => {
+        fetch(props.repo + "/contents/README.md", {
+            headers: {
+                Accept: "application/vnd.github.raw+json",
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch README");
+                }
+                return response.text();
+            })
+            .then((data) => setDescription(data))
+            .catch((err) => console.log(err.message));
+    }, []);
+
     return (
-        <div id={props.projectId}>
-            <h1>{props.projectName ? props.projectName : "No project"}</h1>
+        <div id={props.name}>
+            <h1>{props.name ? props.name : "No project"}</h1>
             <div className="project-info">
                 <nav className="project-info-nav">
                     <a href="#">
@@ -27,16 +45,15 @@ function ProjectInfo(props : ProjectInfoProps) {
                             onClick={() => setActiveItem("contributors")}>Contributors
                         </div>
                     </a>
-                    <a href="#">
-                        <div className={activeItem === "release-log" ? "project-info-item active" : "project-info-item"}
-                             onClick={() => setActiveItem("release-log")}>Release log
+                    <Link to="https://github.com/mateasmario/pie/releases">
+                        <div className={activeItem === "release-log" ? "project-info-item active" : "project-info-item"}>
+                            Binaries
                         </div>
-                    </a>
+                    </Link>
                 </nav>
                 <div className="project-info-content">
-                    {activeItem === "description" ? props.description ? props.description : "No description defined" :
+                    {activeItem === "description" ? description ? <Markdown>{description}</Markdown> : "No description defined" :
                         activeItem === "contributors" ? props.contributors ? props.contributors : "No contributors defined" :
-                            activeItem === "release-log" ? props.releaseLog ? props.releaseLog : "No release log defined":
                                 null }
                 </div>
             </div>
--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -1,38 +1,30 @@
 import ProjectInfo from "./ProjectInfo.tsx";
-import app from "../App.tsx";
 import {useState} from "react";
 
-const [projects, setProjects] = useState([]);
-
-
-const projects = [
-    {
-        projectId: "pie",
-        projectName: "Pie",
-        description: "Lightweight code editor with some third-party features and a pretty design. ",
-        contributors: "Mario-Mihai Mateas",
-        releaseLog: []
-    },
-    {
-        projectId: "requesthor",
-        projectName: "Requesthor",
-        description: "A .NET (Winforms) application that helps you send API requests using a friendly and simple interface. ",
-        contributors: "Mario-Mihai Mateas",
-        releaseLog: []
-    },
-    {
-        projectId: "dehasher",
-        projectName: "Dehasher",
-        description: "Some pretty (inefficient) \"dehashing\" tool written in Python. Uses multiprocessing and Python's hashlib module. Currently working for MD5 and all the SHA hashing methods.",
-        contributors: "Mario-Mihai Mateas",
-        releaseLog: []
-    }
-]
+interface ProjectInfoData {
+    name : string | undefined;
+    repo : string | undefined;
+    contributors : string[] | undefined
+}
 
 function Projects() {
+    const [projects, setProjects] = useState<ProjectInfoData[]>([]);
+
+    // Read from 'static/projects.json'
+    // fetch("../../../static/projects.json")
+    fetch("https://raw.githubusercontent.com/mateasmario/bwxor/master/static/projects.json")
+        .then((response) => {
+           return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            setProjects(data);
+        })
+        .catch((error) => console.error(error));
+
     return (
       <div className="projects">
-          {projects.map((project) => <ProjectInfo {... project} />)}
+          {projects.map((project : ProjectInfoData) => <ProjectInfo {... project} />)}
       </div>
     );
 }
