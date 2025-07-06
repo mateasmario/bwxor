6a7eb8ab6013dd1303f3ca1fc0b105b0d407bb30
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-07-06 11:59:50 +0300

    Added backend dummy project

---
+++ backend/src/test/java/com/bwxor/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwxor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
