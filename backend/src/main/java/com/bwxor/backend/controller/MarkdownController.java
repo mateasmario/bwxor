79d2d7fc0d946c64c0cd5778cbd95392235ee04e
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-07-07 11:50:05 +0300

    Modified backend url

--- backend/src/main/java/com/bwxor/backend/controller/MarkdownController.java
+++ backend/src/main/java/com/bwxor/backend/controller/MarkdownController.java
@@ -10,29 +10,30 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin(origins="*")
 public class MarkdownController {
     @Autowired
     private MarkdownService markdownService;
 
-    @GetMapping("health")
+    @GetMapping("/health")
     public ResponseEntity<String> getHealth() {
         return ResponseEntity.ok("Backend server is healthy.");
     }
 
-    @GetMapping("pages/index")
+    @GetMapping("/pages/index")
     public ResponseEntity<Markdown> getIndex() {
         Markdown indexPage = markdownService.findIndex();
         return ResponseEntity.ok(indexPage);
     }
 
-    @GetMapping("pages/{category}")
+    @GetMapping("/pages/{category}")
     public ResponseEntity<List<MarkdownSummary>> getByCategory(@PathVariable String category) {
         List<MarkdownSummary> indexPage = markdownService.findByCategory(category);
         return ResponseEntity.ok(indexPage);
     }
 
-    @GetMapping("pages/{category}/{slug}")
+    @GetMapping("/pages/{category}/{slug}")
     public ResponseEntity<List<Markdown>> getByCategoryAndSlug(@PathVariable String category, @PathVariable String slug) {
         List<Markdown> indexPage = markdownService.findByCategoryAndSlug(category, slug);
         return ResponseEntity.ok(indexPage);
