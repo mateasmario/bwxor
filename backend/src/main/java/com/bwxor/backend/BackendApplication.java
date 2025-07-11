38165ca63605d735c850037995b03b56a8c37c1e
Mario-MihaiMateaș <mateasmario@ibm.com>
2025-07-11 11:54:35 +0300

    Modified frontend & backend

--- backend/src/main/java/com/bwxor/backend/BackendApplication.java
+++ backend/src/main/java/com/bwxor/backend/BackendApplication.java
@@ -1,8 +1,11 @@
 package com.bwxor.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "API URL")})
 @SpringBootApplication
 public class BackendApplication {
 
