310ae575aef0618bfd6f0525458587d6c9ee156f
Mario-Mihai Mateas <mateasmario@ibm.com>
2025-07-24 17:58:56 +0300

    upd

--- frontend/src/components/item/ItemView.tsx
+++ frontend/src/components/item/ItemView.tsx
@@ -1,55 +1,56 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vs, vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
-import {useTheme} from "../../context/ThemeContext.tsx";
+import { useTheme } from "../../context/ThemeContext.tsx";
+import { Components } from "react-markdown";
 
 interface ItemViewProps {
-    category : string | undefined;
+    category: string | undefined;
 }
 
-function ItemView(props : ItemViewProps) {
-    const {slug} = useParams();
+function ItemView(props: ItemViewProps) {
+    const { slug } = useParams();
     const [markdown, setMarkdown] = useState("Loading...");
-    const {theme} = useTheme();
+    const { theme } = useTheme();
 
     useEffect(() => {
-        fetch("https://bwxor.com/api/pages/" + props.category + "/" + slug)
+        fetch(`https://bwxor.com/api/pages/${props.category}/${slug}`)
             .then((response) => response.json())
-            .then((data) => data[0].content)
-            .then(
-                (data) => setMarkdown(data)
-            )
+            .then((data) => setMarkdown(data[0].content))
             .catch(() => setMarkdown("ItemView with specified name not found."));
-    }, []);
+    }, [props.category, slug]);
+
+    const components: Components = {
+        code({ inline, className, children, ...props } : {
+            inline?: boolean;
+            className?: string;
+            children?:React.ReactNode;
+        }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+                <SyntaxHighlighter
+                    style={theme === "light" ? vs : vscDarkPlus}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                >
+                    {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+            ) : (
+                <code className={className} {...props}>
+                    {children}
+                </code>
+            );
+        },
+    };
 
     return (
-        <>
-            <ReactMarkdown
-                children={markdown}
-                components={{
-                    code({ node, inline, className, children, ...props }) {
-                        const match = /language-(\w+)/.exec(className || "");
-                        return !inline && match ? (
-                            <SyntaxHighlighter
-                                style={theme == "light" ? vs : vscDarkPlus}
-                                language={match[1]}
-                                PreTag="div"
-                                {...props}
-                            >
-                                {String(children).replace(/\n$/, "")}
-                            </SyntaxHighlighter>
-                        ) : (
-                            <code className={className} {...props}>
-                                {children}
-                            </code>
-                        );
-                    },
-                }}
-            />
-        </>
-    )
+        <ReactMarkdown components={components}>
+            {markdown}
+        </ReactMarkdown>
+    );
 }
 
 export default ItemView;
